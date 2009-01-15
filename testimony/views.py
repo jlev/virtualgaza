5ad997cce2a28efdbde2992b8c232dadd8011a1e
jlev <jlev@d9368090-d742-4522-a1b0-5c4bd304c387>
2009-01-15 21:27:26 +0000

    updated models, removed compiled files

--- testimony/views.py
+++ testimony/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.http import Http404
+from testimony.models import Text
+
+def diaryDetail(request, id):
+    try:
+        d = Text.objects.get(pk=id)
+    except Text.DoesNotExist:
+        raise Http404
+    return render_to_response('testimony/diary.html', {'text': d})
