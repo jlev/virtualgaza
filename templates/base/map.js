0341e43c9e78591a53aa3326be412351f79eeda2
jlev <jlev@d9368090-d742-4522-a1b0-5c4bd304c387>
2009-01-23 03:55:19 +0000

    set max extent and min zoom level so map can't slip away

--- templates/base/map.js
+++ templates/base/map.js
@@ -4,8 +4,13 @@ function init() {
 		displayProjection : new OpenLayers.Projection("EPSG:4326"),
 		units : "m",
 		maxResolution : 156543.0339,
-		maxExtent : new OpenLayers.Bounds(-20037508,-20037508,20037508,20037508)
+		maxExtent : new OpenLayers.Bounds(-20037508,-20037508,20037508,20037508), //whole world
+		restrictedExtent : new OpenLayers.Bounds(3801000,3660600,3850000,3711500), //gaza strip
+		minZoomLevel: 10
 	});
+//	map.addControl(new OpenLayers.Control.MousePosition());
+//	map.addControl(new OpenLayers.Control.Permalink('permalink'));
+//	map.addControl(new OpenLayers.Control.LayerSwitcher({div:mapKey}));
 	map.addControl(new OpenLayers.Control.LayerSwitcher());
 	
 	var polygonStyleMap = new OpenLayers.StyleMap(
