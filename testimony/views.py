d8f70b73041cc06d44403a328598d6bcc4858f68
jlev <jlev@d9368090-d742-4522-a1b0-5c4bd304c387>
2009-01-23 04:30:57 +0000

    sorted author list properly

--- testimony/views.py
+++ testimony/views.py
@@ -15,24 +15,21 @@ def deslug(name):
 	return " ".join(bits)
 
 def authors_by_neighborhood(request):
-	theList = []
-	neighborhood_list = Neighborhood.objects.all()
-	for n in neighborhood_list:
-		theList.append(n)
-	author_list = Author.objects.all().select_related()
+	author_list = []
+	all_authors = Author.objects.all().order_by('last_name').select_related()
 	
-	point_list = []
-	for a in author_list:
-		point_list.append(a.location.getJSON())
+	for a in all_authors:
+		author_list.append({'first_name':a.first_name,'last_name':a.last_name,
+			'neighborhood':a.neighborhood.name})
+	
 	return render_to_response('testimony/authors_by_neighborhood.html',dict(mapDict,
-				authors_unsorted=author_list,
-				point_list=point_list),
+				author_list=author_list),
 			context_instance = RequestContext(request))
 
 def author_by_full_name(request, firstName, lastName):
 	"""Gets author by full name"""
 	author = get_object_or_404(Author, first_name__iexact=deslug(firstName), last_name__iexact=deslug(lastName))
-	posts = author.text_set.all()
+	posts = author.text_set.all().order_by('created_date')
 	return render_to_response('testimony/author_detail.html',dict(mapDict,
 				author=author,posts=posts,
 				point_layer_name=author,
@@ -62,8 +59,8 @@ def author_by_last_name(request, lastName):
 def posts_by_author(request, firstName, lastName):
 	first = deslug(firstName)
 	last = deslug(lastName)
-	posts=get_list_or_404(Text,author__first_name__iexact=first,
-					author__last_name__iexact=last,approved=1)
+	posts=Text.objects.all().filter(author__first_name__iexact=first,
+					author__last_name__iexact=last,approved=1).order_by('created_date')
 
 	return render_to_response('testimony/post_list.html',
 						{'first_name':first,'last_name':last,
@@ -73,9 +70,8 @@ def posts_by_author(request, firstName, lastName):
 def posts_by_author_and_year(request, firstName, lastName, year):
 	first = deslug(firstName)
 	last = deslug(lastName)
-	posts=get_list_or_404(Text,author__first_name__iexact=first,
-					author__last_name__iexact=last,
-					created_date__year=year,approved=1)
+	posts=Text.objects.all().filter(author__first_name__iexact=first,
+					author__last_name__iexact=last,created_date__year=year,approved=1).order_by('created_date')
 
 	return render_to_response('testimony/post_list.html',
 						{'first_name':first,'last_name':last,
@@ -86,11 +82,10 @@ def posts_by_author_and_year(request, firstName, lastName, year):
 def posts_by_author_and_month(request, firstName, lastName, year, month):
 	first = deslug(firstName)
 	last = deslug(lastName)
-	posts=get_list_or_404(Text,author__first_name__iexact=first,
+	posts=Text.objects.all().filter(author__first_name__iexact=first,
 					author__last_name__iexact=last,
-					created_date__year=year,
-					created_date__month=month,
-					approved=1)
+					created_date__year=year,created_date__month=month,
+					approved=1).order_by('created_date')
 
 	return render_to_response('testimony/post_list.html',
 						{'first_name':first,'last_name':last,
@@ -101,12 +96,12 @@ def posts_by_author_and_month(request, firstName, lastName, year, month):
 def posts_by_author_and_date(request, firstName, lastName, year, month, day):
 	first = deslug(firstName)
 	last = deslug(lastName)
-	posts=get_list_or_404(Text,author__first_name__iexact=first,
+	posts=Text.objects.all().filter(author__first_name__iexact=first,
 					author__last_name__iexact=last,
 					created_date__year=year,
 					created_date__month=month,
 					created_date__day=day,
-					approved=1)
+					approved=1).order_by('created_date')
 					
 	return render_to_response('testimony/post_list.html',
 						{'first_name':first,'last_name':last,
