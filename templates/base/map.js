35d6dc1583ac943572836e54736f4e9dbe951818
jlev <jlev@d9368090-d742-4522-a1b0-5c4bd304c387>
2009-01-29 23:02:13 +0000

    style redesign

--- templates/base/map.js
+++ templates/base/map.js
@@ -1,91 +1,67 @@
 function init() {
-	map = new OpenLayers.Map('map', {
+	var map = new OpenLayers.Map('map', {
 		projection : new OpenLayers.Projection("EPSG:900913"),
-		displayProjection : new OpenLayers.Projection("EPSG:4326"),
+		displayProjection : new OpenLayers.Projection("EPSG:900913"), //EPSG:4326
 		units : "m",
 		maxResolution : 156543.0339,
 		maxExtent : new OpenLayers.Bounds(-20037508,-20037508,20037508,20037508), //whole world
-		restrictedExtent : new OpenLayers.Bounds(3801000,3660600,3850000,3711500), //gaza strip
-		minZoomLevel: 10
-	});
+		restrictedExtent : new OpenLayers.Bounds(3801000,3660000,3850000,3710500), //gaza strip
+		minZoomLevel: 11});
 //	map.addControl(new OpenLayers.Control.MousePosition());
 //	map.addControl(new OpenLayers.Control.Permalink('permalink'));
-//	map.addControl(new OpenLayers.Control.LayerSwitcher({div:mapKey}));
-	map.addControl(new OpenLayers.Control.LayerSwitcher());
+	map.addControl(new OpenLayers.Control.customLayerSwitcher({'div':OpenLayers.Util.getElement('mapKey'),
+	activeColor:'silver'}));
 	
+	//could pass these in from views, but easy enough to just define them here...
 	var polygonStyleMap = new OpenLayers.StyleMap(
-					{'strokeWidth':1,
-					'strokeColor':'#FF6600',
-					'fillColor':'#FF6600',
-					'fillOpacity':0.1,
-					
+					{strokeWidth:1,
+					strokeColor:'#FF6600',
+					fillColor:'#FF6600',
+					fillOpacity:0.1,
 					pointerEvents: "visiblePainted",
 					label : "${name}",
 					fontColor: "white",
 					fontSize: "10px",
 					fontFamily: "Arial, sans-serif",
 					fontWeight: "bold",
-					labelAlign: "center"
-					});
+					labelAlign: "center"});
 	var pointStyleMap = new OpenLayers.StyleMap(
-					{'pointRadius': 10, //why does this control image size?
-					'fillOpacity':1,
-					'externalGraphic':'{{MEDIA_URL}}pins/male.png'
-					});
+				{pointRadius: 10,
+				fillOpacity:1,
+				externalGraphic:"{{MEDIA_URL}}pins/male.png"});
 	var lineStyleMap = new OpenLayers.StyleMap(
-					{'strokeWidth':3,
-					'strokeColor':'#FFFF00',
-					});
+					{strokeWidth:3,
+					strokeColor:'#FFFF00'});
 	
 	var baseLayer = new OpenLayers.Layer.Google(
-		"Google Aerial",{type: {{ mapType }}, numZoomLevels: 20,sphericalMercator:true,}
+		"Google Aerial",{type: {{ mapType }}, numZoomLevels: 20,sphericalMercator:true,displayInLayerSwitcher:false}
 	);
 	map.addLayer(baseLayer);
 	
 	var geojson_format = new OpenLayers.Format.GeoJSON();
-	var polygon_layer = new OpenLayers.Layer.Vector(" {{ vector_layer_name }} ",{'styleMap': polygonStyleMap});
-
-	{% if poly_list %}
-		{% for poly in poly_list %}
-			var polygon_bounds_{{ forloop.counter }} = {{ poly|safe }}; 
-			var polygon_feature_{{forloop.counter}} = geojson_format.read(polygon_bounds_{{ forloop.counter }});
-			
-			polygon_layer.addFeatures(polygon_feature_{{ forloop.counter }});
+	
+	{% for layer in vectorLayers %}
+		var {{layer.name}}_layer = new OpenLayers.Layer.Vector("{{layer.name}}",{'styleMap':{{layer.styleName}}});
+		{% for object in layer.list %}
+			var {{layer.name}}_{{forloop.counter}} = geojson_format.read({{ object|safe }});
+			{{layer.name}}_layer.addFeatures({{layer.name}}_{{forloop.counter}});
 		{% endfor %}
-		map.addLayer(polygon_layer);
-		map.zoomToExtent(polygon_layer.getDataExtent());
-	{% endif %}
-
-	{% if point_list %}
-		var point_layer = new OpenLayers.Layer.Vector(" {{ point_layer_name }} ",{'styleMap': pointStyleMap});
-		{% for point in point_list %}
-			var point_{{ forloop.counter }} = {{ point|safe }};
-			point_layer.addFeatures(geojson_format.read(point_{{ forloop.counter }}));
-		{% endfor %}
-		map.addLayer(point_layer);
-		{% if not poly_list %}
-			map.zoomToExtent(point_layer.getDataExtent());
-			map.zoomOut();
-			map.zoomOut(); //there isn't imagery at the closest levels
-		{% endif %}
-	{% endif %}
-
-	{% if line_list %}
-		var line_layer = new OpenLayers.Layer.Vector(" {{ line_layer_name }} ",{'styleMap': lineStyleMap});
-		{% for line in line_list %}
-			var line_{{ forloop.counter }} = {{ line|safe }}; 
-			var line_feature_{{forloop.counter}} = geojson_format.read(line_{{ forloop.counter }});
-			line_layer.addFeatures(line_feature_{{ forloop.counter }});
-		{% endfor %}
-		map.addLayer(line_layer);
-	{% endif %}
+		map.addLayer({{layer.name}}_layer);
+		map.zoomToExtent({{layer.name}}_layer.getDataExtent());
+	{%endfor%}
 
 //event handlers
-function goToiBoxLink(feature) {
-	iBox.showURL(feature.attributes.link,'title','options');
+function gotoFloatboxLink(feature) {
+	fb.loadAnchor(feature.attributes.link,'title');
 }
-function goToWindowLink(feature) {
+function gotoWindowLink(feature) {
 	window.location.href = feature.attributes.link;
+}
+function gotoTabLink(feature) {
+	//tabs array is zero based
+	jQuery("#tabs").tabs("url", 2, feature.attributes.link).tabs("select", 2).tabs("load", 2);
+	var linkLocation = new OpenLayers.LonLat(feature.geometry.x,feature.geometry.y);
+	map.moveTo(linkLocation,5); //move and zoom
 }
 function toolTipsOver(feature) {
 	tooltips.show({html:feature.attributes.displayName});
@@ -95,14 +71,10 @@ function toolTipsOut(feature){
 }
 
 //select controls
-tooltips = new OpenLayers.Control.ToolTips({bgColor:"red",textColor :"black", bold : true, opacity : 0.50});
+var tooltips = new OpenLayers.Control.ToolTips({bgColor:"red",textColor :"black", bold : true, opacity : 0.50});
 map.addControl(tooltips);
-
-/*var pointSelectControl = new OpenLayers.Control.SelectFeature(point_layer,
-					{onSelect: toolTipsOver,onUnselect:toolTipsOut,hover:true});*/
-var pointSelectControl = new OpenLayers.Control.newSelectFeature(point_layer,
-					               {onClickSelect: goToiBoxLink,
-					                onHoverSelect: toolTipsOver, onHoverUnselect: toolTipsOut});
+var pointSelectControl = new OpenLayers.Control.newSelectFeature({{tooltipLayerName}}_layer,
+				{onClickSelect:gotoTabLink, onHoverSelect:toolTipsOver, onHoverUnselect:toolTipsOut});
 map.addControl(pointSelectControl);
 pointSelectControl.activate();
-}
+} //end init
