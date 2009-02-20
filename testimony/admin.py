1a68026db09cdf90e01a54abc1ca8ea1b38723d0
jlev <jlev@d9368090-d742-4522-a1b0-5c4bd304c387>
2009-02-20 21:06:41 +0000

    new author admin form

--- testimony/admin.py
+++ testimony/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-import testimony.models
+import django.forms as forms
+from testimony.models import *
+
+class AuthorForm(forms.ModelForm):
+	description = forms.CharField(label="Brief Author Biography",widget=forms.Textarea,help_text="Max length is 500 characters.",required=False)
+	class Meta:
+		model = Author
 
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('first_name','last_name','gender','neighborhood')
 	fieldsets = [
-	 	('Personal', {'fields':['first_name','last_name','gender','picture']}),
+	 	('Personal', {'fields':['first_name','last_name','gender','picture','auto_approve','description','description_short']}),
 		('Contact', {'fields':['email','phone_number']}),
-		('Location', {'fields':['neighborhood','location']}),
+		('Location', {'fields':['neighborhood']}),
 	]
+	form = AuthorForm
 	
 class TextAdmin(admin.ModelAdmin):
 	list_display = ('author','description','created_date','uploaded_date', 'approved')
@@ -23,7 +30,7 @@ class VideoAdmin(admin.ModelAdmin):
 	fieldsets = [
 	    (None, {'fields': ['author']}),
 	    ('Date', {'fields': ['created_date']}),
-	    ('Content', {'fields': ['description','video','thumbnail']}),
+	    ('Content', {'fields': ['description','video']}),
 		('Publication', {'fields': ['approved']}),
 	]	
 
@@ -35,8 +42,12 @@ class AudioAdmin(admin.ModelAdmin):
 	    ('Content', {'fields': ['description','audio']}),
 		('Publication', {'fields': ['approved']}),
 	]
+
+class FeedAdmin(admin.ModelAdmin):
+	fields = ['title','url','regex','default_author']
 	
-admin.site.register(testimony.models.Author, AuthorAdmin)
-admin.site.register(testimony.models.Text,TextAdmin)
-admin.site.register(testimony.models.Video,VideoAdmin)
-admin.site.register(testimony.models.Audio,AudioAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Text,TextAdmin)
+admin.site.register(Video,VideoAdmin)
+admin.site.register(Audio,AudioAdmin)
+admin.site.register(Feed,FeedAdmin)
